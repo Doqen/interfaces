95e60ce936ec1c26c4f6cdbb0dedd423bf950c82
Doqen <86023509+Doqen@users.noreply.github.com>
2023-04-19 22:01:52 +0100

    update

--- Navigation/Rooms/main.js
+++ Navigation/Rooms/main.js
@@ -30,3 +30,9 @@ function btnnext() {
     }
   }
  }
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
